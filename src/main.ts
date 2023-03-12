3a83ab34bf1636dbb0377f76a6b873a3f5bf655d
Julien CLAUDEL <julienclaudel51@outlook.com>
2023-03-12 16:28:10 +0100

    Added env variable

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { PokerModule } from './poker.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PokerModule);
+  const configService = app.get(ConfigService);
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(8080);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- src/poker.module.ts
+++ src/poker.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RoomService } from './room.service';
 import { WebsocketGateway } from './websocket.gateway';
 
 @Module({
+  imports: [ConfigModule.forRoot()],
   providers: [WebsocketGateway, RoomService],
 })
 export class PokerModule {}
