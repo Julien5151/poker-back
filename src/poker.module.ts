c530f312b7acb9b69a80de8ec8940ebddcaf0462
Julien CLAUDEL <julienclaudel51@outlook.com>
2023-06-17 18:04:20 +0200

    Add missing broadcast service

--- src/poker.module.ts
+++ src/poker.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { BroadcastService } from './services/broadcast.service';
 import { RoomService } from './services/room.service';
 import { UserService } from './services/user.service';
 import { WebsocketGateway } from './websocket.gateway';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  providers: [WebsocketGateway, RoomService, UserService],
+  providers: [WebsocketGateway, RoomService, UserService, BroadcastService],
 })
 export class PokerModule {}
